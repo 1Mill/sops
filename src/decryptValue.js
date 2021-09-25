fb5c1ca6d2d70a65faabc7a07a7e9a88c9c32158
1Mill <erik.ekberg@outlook.com>
2021-09-24 20:46:45 -0700

    Only encode values that need to be encoded

--- src/decryptValue.js
+++ src/decryptValue.js
@@ -1,21 +1,24 @@
 const crypto = require('crypto')
 
 const decryptValue = ({ authKey, key, value }) => {
+	// ! Ordering is important
 	const [
 		_,
 		data,
 		iv,
 		tag,
 		type,
-	] = value
-		.match(/^ENC\[AES256_GCM,data:(.+),iv:(.+),tag:(.+),type:(.+)\]/,)
-		.map(item => Buffer.from(item, 'base64'))
+	] = value.match(/^ENC\[AES256_GCM,data:(.+),iv:(.+),tag:(.+),type:(.+)\]/,)
 
-	decryptor = crypto.createDecipheriv('aes-256-gcm', authKey, iv)
-	decryptor.setAuthTag(tag)
+	const dataBase64 = Buffer.from(data, 'base64')
+	const ivBase64 = Buffer.from(iv, 'base64')
+	const tagBase64 = Buffer.from(tag, 'base64')
+
+	decryptor = crypto.createDecipheriv('aes-256-gcm', authKey, ivBase64)
 	decryptor.setAAD(Buffer.from(`${key}:`))
+	decryptor.setAuthTag(tagBase64)
 
-	const cleartext = decryptor.update(data, undefined, 'utf-8') + decryptor.final('utf-8')
+	const cleartext = decryptor.update(dataBase64, undefined, 'utf-8') + decryptor.final('utf-8')
 
 	console.log('!!!!!!!!!!!!!!!!!!!')
 	console.log(cleartext)
