31c6c0fa4b091ccdc7ce4bd207e04a96874ce805
1Mill <erik.ekberg@outlook.com>
2021-09-24 20:47:23 -0700

    Reduce indirection

--- src/decryptValue.js
+++ src/decryptValue.js
@@ -10,15 +10,11 @@ const decryptValue = ({ authKey, key, value }) => {
 		type,
 	] = value.match(/^ENC\[AES256_GCM,data:(.+),iv:(.+),tag:(.+),type:(.+)\]/,)
 
-	const dataBase64 = Buffer.from(data, 'base64')
-	const ivBase64 = Buffer.from(iv, 'base64')
-	const tagBase64 = Buffer.from(tag, 'base64')
-
-	decryptor = crypto.createDecipheriv('aes-256-gcm', authKey, ivBase64)
+	decryptor = crypto.createDecipheriv('aes-256-gcm', authKey, Buffer.from(iv, 'base64'))
 	decryptor.setAAD(Buffer.from(`${key}:`))
-	decryptor.setAuthTag(tagBase64)
+	decryptor.setAuthTag(Buffer.from(tag, 'base64'))
 
-	const cleartext = decryptor.update(dataBase64, undefined, 'utf-8') + decryptor.final('utf-8')
+	const cleartext = decryptor.update(Buffer.from(data, 'base64'), undefined, 'utf-8') + decryptor.final('utf-8')
 
 	console.log('!!!!!!!!!!!!!!!!!!!')
 	console.log(cleartext)
