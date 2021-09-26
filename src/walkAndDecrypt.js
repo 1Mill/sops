f763c8a4672ca16459cecde35095f9b8c349faee
1Mill <erik.ekberg@outlook.com>
2021-09-25 18:09:20 -0700

    Update walkAndDecrypt.js

--- src/walkAndDecrypt.js
+++ src/walkAndDecrypt.js
@@ -1,16 +1,16 @@
 const { decryptScalar } = require('./decryptScalar')
 
-const walkAndDecrypt = ({ aad = '', key, tree }) => {
-	const decryptValue = ({ aad, value }) => {
-		if (Array.isArray(value)) return value.map(v => decryptValue({ aad, value: v }))
-		if (typeof value === 'object') return walkAndDecrypt({ aad, key, tree: value })
-		return decryptScalar({ aad, key, value })
-	}
+const decryptValue = ({ aad, key, value }) => {
+	if (Array.isArray(value)) return value.map(v => decryptValue({ aad, key, value: v }))
+	if (typeof value === 'object') return walkAndDecrypt({ aad, key, tree: value })
+	return decryptScalar({ aad, key, value })
+}
 
+const walkAndDecrypt = ({ aad = '', key, tree }) => {
 	const r = {}
 	Object.entries(tree).forEach(([k, v]) => {
 		if (k === 'sops') return
-		r[k] = decryptValue({ aad: `${aad}${k}:`, value: v })
+		r[k] = decryptValue({ aad: `${aad}${k}:`, key, value: v })
 	})
 	return r
 }
