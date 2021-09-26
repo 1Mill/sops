8035918c18acf72b375dbdc15b0e2dbe02847ce4
1Mill <erik.ekberg@outlook.com>
2021-09-25 18:00:09 -0700

    Rename doValue to decryptValue

--- src/walkAndDecrypt.js
+++ src/walkAndDecrypt.js
@@ -1,8 +1,8 @@
 const { decryptScalar } = require('./decryptScalar')
 
 const walkAndDecrypt = ({ aad = '', key, tree }) => {
-	const doValue = ({ aad, value }) => {
-		if (Array.isArray(value)) return value.map(v => doValue({ aad, value: v }))
+	const decryptValue = ({ aad, value }) => {
+		if (Array.isArray(value)) return value.map(v => decryptValue({ aad, value: v }))
 		if (typeof value === 'object') return walkAndDecrypt({ aad, key, tree: value })
 		return decryptScalar({ aad, key, value })
 	}
@@ -10,7 +10,7 @@ const walkAndDecrypt = ({ aad = '', key, tree }) => {
 	const r = {}
 	Object.entries(tree).forEach(([k, v]) => {
 		if (k === 'sops') return
-		r[k] = doValue({ aad: `${aad}${k}:`, value: v })
+		r[k] = decryptValue({ aad: `${aad}${k}:`, value: v })
 	})
 	return r
 }
